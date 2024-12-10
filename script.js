3c9bdfbec48dfea63cec09ff689332b1e8be3a4a
Meryem Uygurer <hmuygurer@outlook.com>
2024-12-10 22:18:19 +0300

    Styling Updates

---
+++ script.js
@@ -0,0 +1,8 @@
+const task = document.querySelector('todo-input').value;
+const taskArea = document.getElementById('task-ul');
+
+function taskAdd(task){
+    const task = task;
+    const li = document.createElement('li');
+    
+}
